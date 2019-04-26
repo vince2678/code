5fe8f6158fab4088f1fee0bd73f6e49a886b031e
Vincent Zvikaramba <zvikovincent@gmail.com>
2019-04-25 23:55:36 -0400

    c: cbuf: Implement empty/dummy functions
     - To allow tests to compile

--- c/cbuf/circular_buf.c
+++ c/cbuf/circular_buf.c
@@ -1,1 +1,26 @@
 #include "circular_buf.h"
+
+#include <stdio.h> /* NULL */
+
+void *get(struct circular_buf_t *cbuf, int index)
+{
+    return NULL;
+}
+
+void *pop(struct circular_buf_t *cbuf)
+{
+    return NULL;
+}
+int push(struct circular_buf_t *cbuf, void * data)
+{
+    return 0;
+}
+
+struct circular_buf_t *new_circular_buf(int capacity)
+{
+    return NULL;
+}
+
+void delete_circular_buf(struct circular_buf_t *cbuf)
+{
+}
